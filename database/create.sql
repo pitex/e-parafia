d2e903ca14b790994070f923987d1b7fedbd713b
pitex <michalpiekarz09@gmail.com>
2013-12-28 14:18:10 +0100

    chrzty

--- database/create.sql
+++ database/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE parafianie (
-	pesel char(11) PRIMARY KEY,
+	pesel char(11) CONSTRAINT pk_para PRIMARY KEY,
 	imie varchar(100),
 	nazwisko varchar(100),
 	urodzony date,
@@ -9,4 +9,14 @@ CREATE TABLE parafianie (
 CREATE TABLE kaplani (
 	pesel char(11) REFERENCES parafianie(pesel),
 	funkcja varchar(20)
+);
+
+CREATE TABLE chrzty (
+	id numeric CONSTRAINT pk_chrz PRIMARY KEY,
+	pesel_dziecka char(11) CONSTRAINT fk_para_dz REFERENCES parafianie(pesel),
+	pesel_matki char(11),
+	pesel_ojca char(11),
+	pesel_matki_chrz char(11),
+	pesel_ojca_chrz char(11),
+	pesel_kaplana car(11) CONSTRAINT fk_kapl REFERENCES kaplani(pesel)
 );
