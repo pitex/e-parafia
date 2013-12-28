997d7fda487d41953e35af1c11348da982ea390d
Kasia Janocha <kasiajanocha@gmail.com>
2013-12-28 17:10:50 +0100

    create cos tam poczatek

--- database/create.sql
+++ database/create.sql
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 ----   eParafia    ----
 
 CREATE SEQUENCE ID_SEQ increment by 1 start with 1;
-
 
 CREATE TABLE KAPLANI (
 	pesel char(11) CONSTRAINT pk_kapl PRIMARY KEY,
@@ -21,6 +19,8 @@ CREATE TABLE PARAFIANIE (
 );
 
 CREATE TABLE MINISTRANCI (
+	pesel REFERENCES parafianie(pesel),
+	funkcja varchar(100)
 );
 
 CREATE TABLE LEKTORZY (
@@ -29,46 +29,49 @@ CREATE TABLE LEKTORZY (
 CREATE TABLE SZAFARZE (
 );
 
-CREATE TABLE WYDARZENIA (
+CREATE TABLE AKTYWNOSCI_KAPLANOW (
+	id CONSTRAINT pk_wyd PRIMARY KEY,
+	typ char(100) CONSTRAINT wyd_typ ---zeby byl nazwa AKTYWNOSCI_KAPLANOW,
+	data date,
+	pesel_kapl char(11) CONSTRAINT fk_kapl REFERENCES kaplani(pesel)
 );
 
 CREATE TABLE CHRZTY (
-	id numeric CONSTRAINT pk_chrz PRIMARY KEY,
+	id numeric CONSTRAINT fk_chrz REFERENCES AKTYWNOSCI_KAPLANOW(id),
 	pesel_dziecka char(11) CONSTRAINT fk_para_dz REFERENCES parafianie(pesel),
 	pesel_matki char(11),
 	pesel_ojca char(11),
 	pesel_matki_chrz char(11),
 	pesel_ojca_chrz char(11),
-	pesel_kaplana car(11) CONSTRAINT fk_kapl REFERENCES kaplani(pesel)
 );
 
 CREATE TABLE PIERWSZE_KOMUNIE (
-	id numeric CONSTRAINT pk_komu PRIMARY KEY,
+	id numeric CONSTRAINT fk_komu REFERENCES AKTYWNOSCI_KAPLANOW(id),
 	pesel char(11) CONSTRAINT fk_para REFERENCES parafianie(pesel),
 	data date
 );
 
 CREATE TABLE BIERZMOWANIA (
-	id numeric CONSTRAINT pk_bierz PRIMARY KEY,
+	id numeric CONSTRAINT fk_bierz REFERENCES AKTYWNOSCI_KAPLANOW(id),
 	pesel char(11) CONSTRAINT fk_para REFERENCES parafianie(pesel),
-	pesel_swiadka cahr(11),
+	pesel_swiadka char(11),
 	data date
 );
 
 CREATE TABLE SLUBY (
-	id numeric CONSTRAINT pk_malz PRIMARY KEY,
+	id numeric CONSTRAINT fk_slub REFERENCES AKTYWNOSCI_KAPLANOW(id),
 	pesel_zony char(11),
 	pesel_meza char(11),
-	pesel_kapl char(11) CONSTRAINT fk_kapl REFERENCES kaplani(pesel),
 	CONSTRAINT fk_para CHECK(pesel_zony REFERENCES parafianie(pesel) OR pesel_meza REFERENCES parafianie(pesel))
 );
 
 CREATE TABLE POGRZEBY (
-	id numeric CONSTRAINT pk_pogrz PRIMARY KEY,
+	id numeric CONSTRAINT fk_pogrz REFERENCES AKTYWNOSCI_KAPLANOW(id),
 	pesel char(11) CONSTRAINT fk_para REFERENCES parafianie(pesel),
-	pesel_kapl char(11) CONSTRAINT fk_kapl REFERENCES kaplani(pesel),
-	data date
+--dodad trigger usuwajacy parafianina
 );
 
 CREATE TABLE WIZYTY_DUSZPASTERSKIE (
+	id numeric CONSTRAINT fk_wiz REFERENCES AKTYWNOSCI_KAPLANOW(id)
+	adres char(100)
 );
