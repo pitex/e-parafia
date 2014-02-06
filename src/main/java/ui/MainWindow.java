5372f4f0b59ff49ea866a7f81b7c2b58bbd6c537
Kasia Janocha <kasiajanocha@gmail.com>
2014-02-06 19:41:05 +0100

    Panels

--- src/main/java/ui/MainWindow.java
+++ src/main/java/ui/MainWindow.java
@@ -1,5 +1,9 @@
 package ui;
 
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.GridLayout;
+
 import javax.swing.*;
 
 import db.queries.QueryBuilder;
@@ -8,4 +12,53 @@ import db.queries.QueryBuilder;
  * @author Katarzyna Janocha, Michał Piekarz
  */
 public class MainWindow extends JFrame {
+	
+	private JTabbedPane informationPanel() {
+		JTabbedPane tabs = new JTabbedPane();
+		
+		tabs.add("PARAFIANIE", InformationPanels.parafianie());
+		tabs.add("WYDARZENIA", InformationPanels.wydarzenia());
+		tabs.add("KSIEZA", InformationPanels.ksieza());
+		tabs.add("FINANSE", InformationPanels.finanse());
+		
+		return tabs;
+	}
+	
+	private JTabbedPane modificationPanel() {
+		JTabbedPane tabs = new JTabbedPane();
+		
+		tabs.add("PARAFIANIE", ModificationPanels.parafianie());
+		tabs.add("WYDARZENIA", ModificationPanels.wydarzenia());
+		tabs.add("KSIEZA", ModificationPanels.ksieza());
+		tabs.add("FINANSE", ModificationPanels.finanse());		
+		
+		return tabs;		
+	}
+	
+	private JTabbedPane mainWindowTabs() {
+		JTabbedPane tabs = new JTabbedPane();
+		
+		tabs.add("INFORMACJE", informationPanel());
+		
+		tabs.add("MODYFIKACJE", modificationPanel());
+		
+		return tabs;
+	}
+	
+	public MainWindow(String title) {
+		super(title);
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pack();
+		// defaultowa maksymalizacja
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		getContentPane().add(new JLabel(), BorderLayout.CENTER);
+		setVisible(true);
+		
+		setContentPane(mainWindowTabs());
+	}
+	
+	public static void main(String[] args) {
+		MainWindow mainWindow = new MainWindow("e-parafia welcome to!");
+	}
 }
---
+++ src/main/java/ui/ModificationPanels.java
@@ -0,0 +1,29 @@
+package ui;
+
+import javax.swing.JPanel;
+
+public class ModificationPanels {
+	public static JPanel parafianie() {
+		JPanel p = new JPanel();
+		
+		return p;
+	}
+	
+	public static JPanel wydarzenia() {
+		JPanel p = new JPanel();
+		
+		return p;
+	}
+	
+	public static JPanel ksieza() {
+		JPanel p = new JPanel();
+		
+		return p;
+	}
+	
+	public static JPanel finanse() {
+		JPanel p = new JPanel();
+		
+		return p;
+	}
+}
