b175e3937cabfb585daaefa2a21b1dc4c63aa1cf
Kasia Janocha <kasiajanocha@gmail.com>
2014-02-06 20:50:30 +0100

    edit panels

---
+++ src/main/java/panels/EventEditPanel.java
@@ -0,0 +1,10 @@
+package panels;
+
+import javax.swing.JPanel;
+
+/**
+ * @author Katarzyna Janocha, Michał Piekarz
+ */
+public abstract class EventEditPanel extends JPanel {
+
+}
---
+++ src/main/java/panels/WydarzeniaModificationPanel.java
@@ -0,0 +1,10 @@
+package panels;
+
+import javax.swing.JPanel;
+
+/**
+ * @author Katarzyna Janocha, Michał Piekarz
+ */
+public class WydarzeniaModificationPanel extends JPanel {
+
+}
