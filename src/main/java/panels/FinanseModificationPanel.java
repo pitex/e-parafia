b1e535ed0f249babf119afb0cdd6a45c485ff2d3
Kasia Janocha <kasiajanocha@gmail.com>
2014-02-06 23:29:28 +0100

    minor

--- src/main/java/panels/FinanseModificationPanel.java
+++ src/main/java/panels/FinanseModificationPanel.java
@@ -1,10 +1,27 @@
 package panels;
 
+import java.awt.Button;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
 import javax.swing.JPanel;
+
+import common.CommonUI;
 
 /**
  * @author Katarzyna Janocha, Michał Piekarz
  */
 public class FinanseModificationPanel extends JPanel  {
+	public FinanseModificationPanel() {
+		setLayout(new GridBagLayout());
+		
+		GridBagConstraints c = CommonUI.defaultGridBagConstraints();
+		
+		add(addElement(), c);
+	}
 	
+	private Button addElement() {
+		Button b = CommonUI.universalButton("DODAJ OFIARE");
+		return b;
+	}
 }
