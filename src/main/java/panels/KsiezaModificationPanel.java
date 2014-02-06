0a476d097da47bb5e4c6843a175d8d0f01785efd
Kasia Janocha <kasiajanocha@gmail.com>
2014-02-06 23:07:31 +0100

    panels modif - continue

--- src/main/java/panels/KsiezaModificationPanel.java
+++ src/main/java/panels/KsiezaModificationPanel.java
@@ -1,10 +1,42 @@
 package panels;
 
+import java.awt.Button;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
 import javax.swing.JPanel;
+
+import common.CommonUI;
 
 /**
  * @author Katarzyna Janocha, Michał Piekarz
  */
 public class KsiezaModificationPanel extends JPanel {
-
+	public KsiezaModificationPanel() {
+		setLayout(new GridBagLayout());
+		
+		GridBagConstraints c = CommonUI.defaultGridBagConstraints();
+		
+		add(addElement(), c);
+		add(edit(), c);
+		add(remove(), c);
+	}
+	
+	private Button addElement() {
+		Button b = CommonUI.universalButton("DODAJ KAPLANA");
+		
+		return b;
+	}
+	
+	private Button edit() {
+		Button b = CommonUI.universalButton("EDYTUJ DANE KAPLANA");
+		
+		return b;
+	}
+	
+	private Button remove() {
+		Button b = CommonUI.universalButton("USUN KAPLANA");
+		
+		return b;
+	}
 }
--- src/main/java/panels/WydarzeniaModificationPanel.java
+++ src/main/java/panels/WydarzeniaModificationPanel.java
@@ -1,10 +1,61 @@
 package panels;
 
+import java.awt.Button;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
 import javax.swing.JPanel;
+
+import common.CommonUI;
 
 /**
  * @author Katarzyna Janocha, Michał Piekarz
  */
 public class WydarzeniaModificationPanel extends JPanel {
+	public WydarzeniaModificationPanel() {
+		setLayout(new GridBagLayout());
+		
+		GridBagConstraints c = CommonUI.defaultGridBagConstraints();
 
+		add(remove(), c);
+		add(addChrzest(), c);
+		add(addBierzmowanie(), c);
+
+	}
+	
+	private Button remove() {
+		Button b = CommonUI.universalButton("DODAJ BIERZMOWANIE");
+		
+		return b;
+	}
+	
+	private Button addChrzest() {
+		Button b = CommonUI.universalButton("DODAJ CHRZEST");
+		
+		return b;
+	}
+	
+	private Button addKomunia() {
+		Button b = CommonUI.universalButton("DODAJ KOMUNIE SWIETA");
+		
+		return b;
+	}
+	
+	private Button addBierzmowanie() {
+		Button b = CommonUI.universalButton("DODAJ BIERZMOWANIE");
+		
+		return b;
+	}
+
+	private Button addSlub() {
+		Button b = CommonUI.universalButton("DODAJ SLUB");
+		
+		return b;
+	}
+	
+	private Button addPogrzeb() {
+		Button b = CommonUI.universalButton("DODAJ POGRZEB");
+		
+		return b;
+	}
 }
